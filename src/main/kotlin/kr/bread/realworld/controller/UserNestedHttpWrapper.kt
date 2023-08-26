cb72189f681f4ff1f092ce228f60131d988b3d76
bread <thayer2392@gmail.com>
2023-08-26 10:42:02 +0900

    doc: write comment on UserNestedHttpWrapper.kt

--- src/main/kotlin/kr/bread/realworld/controller/UserNestedHttpWrapper.kt
+++ src/main/kotlin/kr/bread/realworld/controller/UserNestedHttpWrapper.kt
@@ -1,5 +1,19 @@
 package kr.bread.realworld.controller
 
+/**
+ * created for wrapping json request & response
+ *
+ * Example
+ *
+ * {
+ *   "user":{
+ *     "email": "jake@jake.jake",
+ *     "bio": "I like to skateboard",
+ *     "image": "https://i.stack.imgur.com/xHWG8.jpg"
+ *   }
+ * }
+ *
+ */
 data class UserNestedHttpWrapper<T>(
     val user: T
 )
